02f0d5c12dbe512d3ca99fae8ec3563cb88308d4
Kevin Pfeifer <kevin.pfeifer@edu.fh-joanneum.at>
2015-11-23 21:39:00 +0100

    Changed result-file naming and structure
    updated README.md

--- src/at/fhj/itm/pswe/pagecrawler/linkcrawler/Init_LinkCrawler.java
+++ src/at/fhj/itm/pswe/pagecrawler/linkcrawler/Init_LinkCrawler.java
@@ -1,5 +1,11 @@
 package at.fhj.itm.pswe.pagecrawler.linkcrawler;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.util.Calendar;
+
 import edu.uci.ics.crawler4j.crawler.CrawlConfig;
 import edu.uci.ics.crawler4j.crawler.CrawlController;
 import edu.uci.ics.crawler4j.fetcher.PageFetcher;
@@ -8,6 +14,8 @@ import edu.uci.ics.crawler4j.robotstxt.RobotstxtServer;
 import edu.uci.ics.crawler4j.url.WebURL;
 
 public class Init_LinkCrawler {
+	
+	private String filename;
 
 	public Init_LinkCrawler(String url, int depth) throws Exception{
 		
@@ -41,6 +49,24 @@ public class Init_LinkCrawler {
 		
 		controller.setCustomData(startUrl);
 
+		final Calendar cal = Calendar.getInstance();
+
+		filename = url.replace(".", "_").replace("http://", "") + "_" + (cal.get(Calendar.MONTH) + 1) + "_"
+				+ cal.get(Calendar.DAY_OF_MONTH) + "_" + cal.get(Calendar.YEAR) + "-" + cal.get(Calendar.HOUR) + "_"
+				+ cal.get(Calendar.MINUTE) + ".txt";
+		String path_to_file = crawlStorageFolder + filename;
+		File f = new File(path_to_file);
+		PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter(path_to_file, true)));
+		// Schreibe bei erstellen der Datei wichtigste Daten in die ersten 2
+		// Zeilen
+		if (f.length() == 0) {
+			out.println(url);
+			out.println(cal.getTime().toString());
+		}
+		if (out != null) {
+			out.close();
+		}
+		
 		/*
 		 * Start the crawl. This is a blocking operation, meaning that your code
 		 * will reach the line after this only when crawling is finished.
@@ -48,6 +74,10 @@ public class Init_LinkCrawler {
 		controller.start(MyJCrawler.class, numberOfCrawlers);
 		System.out.println("Finished");
 
+	}
+	
+	public String getFilename(){
+		return filename;
 	}
 
 }
--- src/at/fhj/itm/pswe/pagecrawler/linkcrawler/MyJCrawler.java
+++ src/at/fhj/itm/pswe/pagecrawler/linkcrawler/MyJCrawler.java
@@ -1,17 +1,16 @@
 package at.fhj.itm.pswe.pagecrawler.linkcrawler;
 
 import java.io.BufferedWriter;
-import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Calendar;
 import java.util.Set;
 import java.util.regex.Pattern;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 
-import edu.uci.ics.crawler4j.crawler.CrawlConfig;
 import edu.uci.ics.crawler4j.crawler.Page;
 import edu.uci.ics.crawler4j.crawler.WebCrawler;
 import edu.uci.ics.crawler4j.parser.HtmlParseData;
@@ -19,69 +18,73 @@ import edu.uci.ics.crawler4j.url.WebURL;
 
 public class MyJCrawler extends WebCrawler {
 
-	private final static Pattern FILTERS = Pattern.compile(".*(\\.(css|js|gif|jpg"
-			+ "|png|mp3|mp3|zip|gz))$");
+	private final static Pattern FILTERS = Pattern.compile(".*(\\.(css|js|gif|jpg" + "|png|mp3|mp3|zip|gz))$");
+
+	private final static Calendar cal = Calendar.getInstance();
 
 	/**
-	 * This method receives two parameters. The first parameter is the page
-	 * in which we have discovered this new url and the second parameter is
-	 * the new url. You should implement this function to specify whether
-	 * the given url should be crawled or not (based on your crawling logic).
-	 * In this example, we are instructing the crawler to ignore urls that
-	 * have css, js, git, ... extensions and to only accept urls that start
-	 * with "http://www.ics.uci.edu/". In this case, we didn't need the
-	 * referringPage parameter to make the decision.
+	 * This method receives two parameters. The first parameter is the page in
+	 * which we have discovered this new url and the second parameter is the new
+	 * url. You should implement this function to specify whether the given url
+	 * should be crawled or not (based on your crawling logic). In this example,
+	 * we are instructing the crawler to ignore urls that have css, js, git, ...
+	 * extensions and to only accept urls that start with
+	 * "http://www.ics.uci.edu/". In this case, we didn't need the referringPage
+	 * parameter to make the decision.
 	 */
 	@Override
 	public boolean shouldVisit(Page referringPage, WebURL url) {
 		String href = url.getURL().toLowerCase();
-		WebURL seedUrl=(WebURL)this.myController.getCustomData();
-		return !FILTERS.matcher(href).matches()
-				&& url.getDomain().equals(seedUrl.getDomain());
+		WebURL seedUrl = (WebURL) this.myController.getCustomData();
+		return !FILTERS.matcher(href).matches() && url.getDomain().equals(seedUrl.getDomain());
 	}
 
 	/**
-	 * This function is called when a page is fetched and ready
-	 * to be processed by your program.
+	 * This function is called when a page is fetched and ready to be processed
+	 * by your program.
 	 */
 	@Override
 	public void visit(Page page) {
 		String url = page.getWebURL().getURL();
 		System.out.println("URL: " + url);
+		String domain = page.getWebURL().getDomain();
+		String subdomain = page.getWebURL().getSubDomain();
+		String total_domain = subdomain + "." + domain;
+
+		System.out.println("Subdomain: " + subdomain + "| Domain: " + domain);
 
 		if (page.getParseData() instanceof HtmlParseData) {
 			HtmlParseData htmlParseData = (HtmlParseData) page.getParseData();
-			
+
 			Document doc = Jsoup.parseBodyFragment(htmlParseData.getHtml());
-			getPageText(doc);
+			getPageText(doc, total_domain);
 			Set<WebURL> links = htmlParseData.getOutgoingUrls();
 
-			
 			System.out.println("Number of outgoing links: " + links.size());
-
 		}
 	}
 
-
-	public void getPageText(Document doc){		
+	public void getPageText(Document doc, String domain) {
 		PrintWriter out = null;
+		String filename = domain.replace(".", "_") + "_" + (cal.get(Calendar.MONTH) + 1) + "_"
+				+ cal.get(Calendar.DAY_OF_MONTH) + "_" + cal.get(Calendar.YEAR) + "-" + cal.get(Calendar.HOUR) + "_"
+				+ cal.get(Calendar.MINUTE) + ".txt";
+		String path_to_file = getMyController().getConfig().getCrawlStorageFolder() + filename;
 		try {
-			out = new PrintWriter(new BufferedWriter(new FileWriter(getMyController().getConfig().getCrawlStorageFolder() + "result.txt",true)));
+			out = new PrintWriter(new BufferedWriter(new FileWriter(path_to_file, true)));
 			// Falls kein HTML gelesen werden kann (Bsp.: XML)
-			if(doc.body() != null){
+			if (doc.body() != null) {
 				out.println(doc.body().text());
 			}
-		}catch (IOException e) {
-			//exception handling left as an exercise for the reader
+		} catch (IOException e) {
+			// exception handling left as an exercise for the reader
 			System.err.println("Error with FileHandling: " + e.getMessage());
-		}finally{
-			if(out != null){
+		} finally {
+			if (out != null) {
 				out.close();
 			}
 		}
 
 	}
-	
-	
-	
+
 }
