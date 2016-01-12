209a51a0e90f1d463a421ee24079f40eac8752be
Kevin Pfeifer <kevin.pfeifer@edu.fh-joanneum.at>
2016-01-12 09:07:41 +0100

    Updated pom.xml with new Selenium Dependency

--- test/at/fhj/itm/pswe/unittest/rest/WordMockTest.java
+++ test/at/fhj/itm/pswe/unittest/rest/WordMockTest.java
@@ -1,15 +1,10 @@
 package at.fhj.itm.pswe.unittest.rest;
 
-import junit.framework.Assert;
-
 import org.junit.Test;
 
-import at.fhj.itm.pswe.dao.WordDao;
 import at.fhj.itm.pswe.dao.interfaces.IWord;
 import at.fhj.itm.pswe.model.Word;
-import at.fhj.itm.pswe.rest.WordEndpoint;
 import static org.easymock.EasyMock.createMock;
-import static org.easymock.EasyMock.expect;
 import static org.easymock.EasyMock.replay;
 import static org.easymock.EasyMock.verify;
 
