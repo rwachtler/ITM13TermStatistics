ddc129adb2c4f335ee1e3fd871256d6d8ab75ab4
Kevin Pfeifer <kevin.pfeifer@edu.fh-joanneum.at>
2016-01-16 16:36:52 +0100

    testReadWebsite working

--- test/at/fhj/itm/pswe/unittest/dao/WebsiteMockTest.java
+++ test/at/fhj/itm/pswe/unittest/dao/WebsiteMockTest.java
@@ -5,7 +5,12 @@ import static org.easymock.EasyMock.expect;
 import static org.easymock.EasyMock.replay;
 import static org.easymock.EasyMock.verify;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.EntityManager;
+import javax.persistence.Query;
+import javax.persistence.TypedQuery;
 
 import org.easymock.EasyMock;
 import org.junit.Test;
@@ -16,7 +21,7 @@ import at.fhj.itm.pswe.model.Website;
 public class WebsiteMockTest {
 	
 	@Test
-	public void deleteWebsite(){
+	public void testdeleteWebsite(){
 		//SETUP 
 		Website w=new Website();
 		w.setId(1);
@@ -47,14 +52,14 @@ public class WebsiteMockTest {
 	}
 	
 	@Test
-	public void createWebsite(){
+	public void testcreateWebsite(){
 		Website w = new Website();
 		w.setDomain("http://test.at");
 		w.setDescription("Test");
 		w.setCrawldepth(1);
 		w.setActive(true);
-		
-		WebsiteDao wDao=new WebsiteDao();
+		w.setLast_crawldate("1970-01-01");
+		w.setId(1);
 		
 		EntityManager mockEm = EasyMock.createMock(EntityManager.class);
 		
@@ -64,12 +69,91 @@ public class WebsiteMockTest {
 		//set to replay state
 		replay(mockEm);
 		//Set Mock Object
+
+		WebsiteDao wDao=new WebsiteDao();
 		wDao.setEntityManager(mockEm);
 
 		//TEST
 		Website result = wDao.createWebsite("http://test.at", "Test", 1);
 
 		//Verify
+		verify(mockEm);
+	}
+	
+	@Test
+	public void testreadWebsite(){
+		//SETUP 
+		Website w=new Website();
+		w.setId(1);
+		w.setDomain("http://test.at");
+		w.setActive(true);
+		w.setDescription("Test");
+		w.setLast_crawldate("2015-05-05");
+		
+		List<Object[]> returnList = new ArrayList<Object[]>();
+		Object[] retArr= new Object[5];
+		retArr[0]=w.getId();
+		retArr[1]=w.getDomain();
+		retArr[2]=w.getLast_crawldate();
+		retArr[3]=w.getDescription();
+		retArr[4]=1;
+		returnList.add(retArr);
+		
+		//QueryMock
+		Query mockedQuery = createMock(Query.class);
+		expect(mockedQuery.setParameter("url", w.getDomain())).andReturn(mockedQuery);
+		expect(mockedQuery.getResultList()).andReturn(returnList);
+		replay(mockedQuery);
+		
+		EntityManager mockEm = createMock(EntityManager.class);
+		expect(mockEm.createQuery(
+				"SELECT ws.id, ws.domain, ws.last_crawl_date, ws.description, ws.active FROM Website ws WHERE ws.domain = :url")
+				).andReturn(mockedQuery);
+		replay(mockEm);
+		
+
+		WebsiteDao wDao=new WebsiteDao();
+		wDao.setEntityManager(mockEm);
+		
+		//TEST SETUP
+		wDao.readWebsite(w.getDomain());
+		
+		//VERIFY
+		verify(mockEm);
+		
+	}
+	
+	@Test
+	public void testfindAllWebsites(){
+		//SETUP 
+		List<Website> expected = new ArrayList<>();
+		Website w = new Website();
+		w.setId(1);
+		w.setDomain("http://test.at");
+		w.setActive(true);
+		w.setDescription("Test");
+		w.setLast_crawldate("2015-05-05");
+		expected.add(w);
+		
+		//QueryMock
+		TypedQuery<Website> mockedQuery = createMock(TypedQuery.class);
+		expect(mockedQuery.getResultList()).andReturn(expected);
+		replay(mockedQuery);
+		
+		EntityManager mockEm = createMock(EntityManager.class);
+		expect(mockEm.createQuery(
+				"SELECT DISTINCT w FROM Website w ORDER BY w.id", Website.class)
+				).andReturn(mockedQuery);
+		replay(mockEm);
+		
+
+		WebsiteDao wDao=new WebsiteDao();
+		wDao.setEntityManager(mockEm);
+		
+		//TEST SETUP
+		wDao.findAllWebsites();
+		
+		//VERIFY
 		verify(mockEm);
 	}
 
