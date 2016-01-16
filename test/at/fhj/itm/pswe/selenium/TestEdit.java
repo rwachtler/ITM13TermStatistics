e7d26cc70b90b2097f57e93144a327c402c60520
koeckman13 <manuel27492@live.at>
2016-01-16 20:28:36 +0100

    testedit
    some flaws with data tables test fails working on it

--- test/at/fhj/itm/pswe/selenium/TestEdit.java
+++ test/at/fhj/itm/pswe/selenium/TestEdit.java
@@ -2,6 +2,8 @@ package at.fhj.itm.pswe.selenium;
 
 import java.util.concurrent.TimeUnit;
 
+import junit.framework.Assert;
+
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -11,7 +13,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class TestEdit {
-	private FirefoxDriver selenium;
+	private static FirefoxDriver selenium;
 	private String url;
 	@Before
 	public void setUp() throws Exception {
@@ -34,11 +36,35 @@ public class TestEdit {
 	public void testEditSite(){
 		addSite();
 		selenium.get("http://localhost:8080/TermStatistics/");
-		//TODO: ein Element aus der Tabelle wählen->auf Edit-> active änderen-> dann alles aus Tabelle ausgeben lassen und prüfen ob es beim richtigen umgestellt worden ist
+		selenium.findElementByXPath("//table[@id='site-list-table']/tbody/tr/td[2]").click();
+		selenium.findElementByLinkText("Edit").click();
+		selenium.findElement(By.id("DTE_Field_active_0")).click();
+		selenium.findElementByCssSelector("button.btn").click();
+		
+		int startnumber;
+		String checkamount=selenium.findElement(By.id("site-list-table")).getText();
+		System.out.println(checkamount);
+		String[] lines=checkamount.split("\n");
+		startnumber=lines.length;
+		System.out.println(startnumber);
+		
+		selenium.navigate().refresh();
+		
+	String output=selenium.findElement(By.id("site-list-table")).getText();
+	
+	if(output.contains("true")){
+		Assert.assertTrue(true);
+	}else{
+		Assert.assertFalse("The rigth details were not found!",true);
 	}
+	
+	
+	}
+	
+
 
 	@AfterClass
-	public void tearDown() throws Exception {
+	public static void tearDown() throws Exception {
 		selenium.get("http://localhost:8080/TermStatistics/rest/website/koeckman.at/delete");
 		selenium.quit();
 	}
